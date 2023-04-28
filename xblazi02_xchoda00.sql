2b70f02ed0e64a08de77ed1efea2ed819f8534b0
IvanIV100 <Ivan.iv.chodak@gmail.com>
2023-04-28 15:39:06 +0200

    Added: Trigger that checks if we have mechanic with given specialization as TYP_OPRAVY Changed: Oprava table has new column POPIS_OPRAVY pre detailny popis. TYP_OPRAVY je teraz zhodny s Mech Spec.

--- xblazi02_xchoda00.sql
+++ xblazi02_xchoda00.sql
@@ -50,7 +50,8 @@ CREATE TABLE OBJEDNAVKA(
 
 CREATE TABLE OPRAVA(
     ID_OPR NUMBER DEFAULT op_id_seq.NEXTVAL PRIMARY KEY,
-    TYP_OPRAVY VARCHAR(200),
+    TYP_OPRAVY VARCHAR(30),
+    POPIS_OPRAVY VARCHAR(200),
     MISTO VARCHAR(30),  -- atribut, protože tam mechanik může zadat cokoli
 
     REF_OBJ INT NOT NULL,
@@ -131,6 +132,22 @@ BEGIN
   :NEW.PRIJMENI := INITCAP(LOWER(:NEW.PRIJMENI));
 END;
 
+---Controls if there is a mechnic for given type of repair---
+CREATE OR REPLACE TRIGGER trg_check_oprava_specialization
+AFTER INSERT ON OPRAVA
+FOR EACH ROW
+DECLARE
+  v_count NUMBER := 0;
+BEGIN
+  SELECT COUNT(*) INTO v_count
+  FROM MECHANIK
+  WHERE SPECIALIZACE = :NEW.TYP_OPRAVY;
+
+  IF v_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001, 'No mechanic found with matching specialization.');
+  END IF;
+END;
+
 
 
 
@@ -150,14 +167,23 @@ INSERT INTO OBJEDNAVKA VALUES(DEFAULT, 'klk-753', 'Honda', 'Civic', 'INV-10012',
 INSERT INTO OBJEDNAVKA VALUES(DEFAULT, 'OLI-631', 'Toyota', 'Hillux', 'INV-1112', 4);
 INSERT INTO OBJEDNAVKA VALUES(DEFAULT, 'GHI-789', 'Peugeot', '308', 'INV-1242', 5);
 
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Výměna brzdových destiček', 'Slot1', 1);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Výměna oleje a filtru', 'Slot2', 2);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Geometria', 'Slot3', 3);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Kontrola pred STK', 'Slot1', 4);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Výměna rozvodů', 'Slot2', 5);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Výměna rozvodů', 'Slot3', 6);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Přezutí pneu na zimní - zákazník má vlastní', 'Rampa', 2);
-INSERT INTO OPRAVA VALUES(DEFAULT, 'Výměna rozvodů - zákazník chce rozvody zkontrolovat, zavolat na tel. a domluvit se co dále', 'Díra', 3);
+INSERT INTO MECHANIK VALUES(7493, 'john', 'Smith', '+1-1234567890', 'johnsmith@example.com', '123 Main St', 'Apt 456', 'Anytown', 12345, DATE '2022-01-01', DATE '1980-01-01', 'Engine');
+INSERT INTO MECHANIK VALUES(9102, 'Jane', 'Doe', '+1-2345678901', 'janedoe@example.com', '456 First St', 'Suite 789', 'Anycity', 23456, DATE '2022-02-01', DATE '1985-02-01', 'Brakes');
+INSERT INTO MECHANIK VALUES(2483, 'Bob', 'Johnson', '+1-345678901', 'bobjohnson@example.com', '789 Second St', 'Unit 123', 'Somewhere', 34567, DATE '2022-03-01', DATE '1990-03-01', 'Suspension');
+INSERT INTO MECHANIK VALUES(6749, 'janko', 'Mrin', '+420-123456789', 'janni@example.com', '13 jankoByva', 'Apt 13', 'Anytown', 12345, DATE '2023-01-01', DATE '1986-01-01', 'Electronics');
+INSERT INTO MECHANIK VALUES(7649, 'Mima', 'Nima', '+421-234567890', 'mmimma@example.com', '15 Svätého Vila', 'Suite 7', 'Anycity', 23456, DATE '2023-02-01', DATE '1975-02-01', 'Bottom');
+INSERT INTO MECHANIK VALUES(3489, 'Kubo', 'Smrad', '+421-345678901', 'Kubbo@example.com', '78 Seco', 'Unit 12', 'Somewhere', 34567, DATE '2023-03-01', DATE '1995-03-01', 'Bodywork');
+INSERT INTO MECHANIK VALUES(1234, 'Adam', 'Březík', '735345928', 'adambrezik@samohylmb.cz', 'Závodní', '14', 'Zlín', 58612, DATE '2020-04-19', DATE '1996-09-02', NULL);
+
+
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Brakes', 'Výměna brzdových destiček', 'Slot1', 1);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Engine', 'Výměna oleje a filtru', 'Slot2', 2);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Suspension','Geometria', 'Slot3', 3);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Bodywork' ,'Kontrola pred STK', 'Slot1', 4);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Electronics','Výměna rozvodů', 'Slot2', 5);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Electronics','Výměna rozvodů', 'Slot3', 6);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Bottom','Přezutí pneu na zimní - zákazník má vlastní', 'Rampa', 2);
+INSERT INTO OPRAVA VALUES(DEFAULT, 'Electronics','Výměna rozvodů - zákazník chce rozvody zkontrolovat, zavolat na tel. a domluvit se co dále', 'Díra', 3);
 
 INSERT INTO MATERIAL VALUES(DEFAULT, 'Řetězová pila', '123456', 'Elektrická', 'STIHL', 5000);
 INSERT INTO MATERIAL VALUES(DEFAULT, 'Ocelová trubka', '789012', 'Konstrukční', 'Svoboda Steel', 10);
@@ -175,13 +201,6 @@ INSERT INTO TERMIN VALUES(DEFAULT, DATE '2022-07-12', '12:00', 5);
 INSERT INTO TERMIN VALUES(DEFAULT, DATE '2022-03-31', '10:00', 6);
 INSERT INTO TERMIN VALUES(DEFAULT, DATE '2023-03-31', '14:30', 8);
 
-INSERT INTO MECHANIK VALUES(7493, 'john', 'Smith', '+1-1234567890', 'johnsmith@example.com', '123 Main St', 'Apt 456', 'Anytown', 12345, DATE '2022-01-01', DATE '1980-01-01', 'Oprava motoru');
-INSERT INTO MECHANIK VALUES(9102, 'Jane', 'Doe', '+1-2345678901', 'janedoe@example.com', '456 First St', 'Suite 789', 'Anycity', 23456, DATE '2022-02-01', DATE '1985-02-01', 'Oprava brzd');
-INSERT INTO MECHANIK VALUES(2483, 'Bob', 'Johnson', '+1-345678901', 'bobjohnson@example.com', '789 Second St', 'Unit 123', 'Somewhere', 34567, DATE '2022-03-01', DATE '1990-03-01', 'Oprava odpružení');
-INSERT INTO MECHANIK VALUES(6749, 'janko', 'Mrin', '+420-123456789', 'janni@example.com', '13 jankoByva', 'Apt 13', 'Anytown', 12345, DATE '2023-01-01', DATE '1986-01-01', 'Výměna rozvodů');
-INSERT INTO MECHANIK VALUES(7649, 'Mima', 'Nima', '+421-234567890', 'mmimma@example.com', '15 Svätého Vila', 'Suite 7', 'Anycity', 23456, DATE '2023-02-01', DATE '1975-02-01', 'Podvozek');
-INSERT INTO MECHANIK VALUES(3489, 'Kubo', 'Smrad', '+421-345678901', 'Kubbo@example.com', '78 Seco', 'Unit 12', 'Somewhere', 34567, DATE '2023-03-01', DATE '1995-03-01', 'Oprava karoserie');
-INSERT INTO MECHANIK VALUES(1234, 'Adam', 'Březík', '735345928', 'adambrezik@samohylmb.cz', 'Závodní', '14', 'Zlín', 58612, DATE '2020-04-19', DATE '1996-09-02', NULL);
 
 INSERT INTO VZTAH_MECH_TER VALUES(200, 4, 1, 9102);
 INSERT INTO VZTAH_MECH_TER VALUES(150, 3, 2, 2483);
